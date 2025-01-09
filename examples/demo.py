7b4e1687b8114ced618c38a0d4948bfa619b8bde
Zvonimir <zvonimir@Zvonimirs-MacBook-Pro.local>
2025-01-09 15:35:07 +0100

    Update demo

--- examples/demo.py
+++ examples/demo.py
@@ -66,70 +66,6 @@ print_json(info_result)
 
 divider()
 
-# formatted_assertions = dkg.assertion.format_graph(content)
-# print("======================== ASSET FORMATTED")
-# print_json(formatted_assertions)
-
-# divider()
-
-# public_assertion_id = dkg.assertion.get_public_assertion_id(content)
-# print("======================== PUBLIC ASSERTION ID (MERKLE ROOT) CALCULATED")
-# print(public_assertion_id)
-
-# divider()
-
-# public_assertion_size = dkg.assertion.get_size(content)
-# print("======================== PUBLIC ASSERTION SIZE CALCULATED")
-# print(public_assertion_size)
-
-# divider()
-
-# public_assertion_triples_number = dkg.assertion.get_triples_number(content)
-# print("======================== PUBLIC ASSERTION TRIPLES NUMBER CALCULATED")
-# print(public_assertion_triples_number)
-
-# divider()
-
-# public_assertion_chunks_number = dkg.assertion.get_chunks_number(content)
-# print("======================== PUBLIC ASSERTION CHUNKS NUMBER CALCULATED")
-# print(public_assertion_chunks_number)
-
-# divider()
-
-# bid_suggestion = dkg.network.get_bid_suggestion(
-#     public_assertion_id,
-#     public_assertion_size,
-#     2,
-# )
-# print("======================== BID SUGGESTION CALCULATED")
-# print(bid_suggestion)
-
-# divider()
-
-# current_allowance = dkg.asset.get_current_allowance()
-# print("======================== GET CURRENT ALLOWANCE")
-# print(current_allowance)
-
-# divider()
-
-# allowance_increase = dkg.asset.increase_allowance(bid_suggestion)
-# print("======================== INCREASE ALLOWANCE")
-# print(allowance_increase)
-
-# divider()
-
-# allowance_decrease = dkg.asset.decrease_allowance(bid_suggestion // 3)
-# print("======================== DECREASE ALLOWANCE")
-# print(allowance_decrease)
-
-# divider()
-
-# allowance_set = dkg.asset.set_allowance(bid_suggestion)
-# print("======================== SET ALLOWANCE")
-# print(allowance_set)
-
-divider()
-
 start_time = time.time()
 create_asset_result = dkg.asset.create(
     content=content,
@@ -140,46 +76,8 @@ create_asset_result = dkg.asset.create(
 )
 print(f"======================== ASSET CREATED in {time.time() - start_time} seconds")
 print_json(create_asset_result)
+
 divider()
-
-# validate_ual = dkg.asset.is_valid_ual(create_asset_result["UAL"])
-# print("======================== VALIDATE UAL")
-# print(f"Is {create_asset_result['UAL']} a valid UAL: {validate_ual}")
-# divider()
-
-# owner_result = dkg.asset.get_owner(create_asset_result["UAL"])
-# print("======================== GET ASSET OWNER")
-# print(owner_result)
-# divider()
-
-# get_asset_result = dkg.asset.get(create_asset_result["UAL"])
-# print("======================== ASSET RESOLVED")
-# print_json(get_asset_result)
-# divider()
-
-# get_latest_asset_result = dkg.asset.get(create_asset_result["UAL"], "latest", "all")
-# print("======================== ASSET LATEST RESOLVED")
-# print_json(get_latest_asset_result)
-# divider()
-
-# get_latest_finalized_asset_result = dkg.asset.get(
-#     create_asset_result["UAL"], "latest_finalized", "all"
-# )
-# print("======================== ASSET LATEST FINALIZED RESOLVED")
-# print_json(get_latest_finalized_asset_result)
-# divider()
-
-# get_first_state_by_index = dkg.asset.get(create_asset_result["UAL"], 0, "all")
-# print("======================== ASSET FIRST STATE (GET BY STATE INDEX) RESOLVED")
-# print_json(get_first_state_by_index)
-# divider()
-
-# get_first_state_by_hash = dkg.asset.get(
-#     create_asset_result["UAL"], create_asset_result["publicAssertionId"], "all"
-# )
-# print("======================== ASSET FIRST STATE (GET BY STATE HASH) RESOLVED")
-# print_json(get_first_state_by_hash)
-# divider()
 
 query_operation_result = dkg.graph.query(
     """
